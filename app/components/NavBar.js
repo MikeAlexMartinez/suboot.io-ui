b070cc48f1bf107a407a48a45e427992160c498a
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2018-03-22 17:55:53 +0000

    dry-ed up isActive function used in navbar links

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,36 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 require('../../stylesheets/components/NavBar.scss');
 
-const isActive = (str) => window.location.pathname === str;
+const isActive = (str) => 
+  window.location.pathname === str
+    ? 'navlink active'
+    : 'navlink';
 
 const NavBar = () => (
   <div className='nav-bar'>
     <Link 
-      className={
-        isActive('/general')
-          ? 'navlink active'
-          : 'navlink'
-      } 
+      className={isActive('/general')}
       to='/general'
     >
       General
     </Link>
     <Link
-      className={
-        isActive('/explore')
-          ? 'navlink active'
-          : 'navlink'
-      } 
+      className={isActive('/explore')} 
       to='/explore'
     >
       Explore
     </Link>
     <Link 
-      className={
-        isActive('/leaguetables')
-          ? 'navlink active'
-          : 'navlink'
-      } 
+      className={isActive('/leaguetables')} 
       to='/leaguetables'
     >
       League Tables
